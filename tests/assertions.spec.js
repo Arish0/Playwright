ba40f9b49702f5e4ae360e69fcb268e5b9318082
Harish <arish4703@gmail.com>
2025-04-22 01:00:48 +0530

    Adding assertions to playwright

---
+++ tests/assertions.spec.js
@@ -0,0 +1,30 @@
+const { test, expect } = require('@playwright/test');
+
+test('assertions', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
+  await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await expect(page).toHaveTitle('OrangeHRM');
+
+ 
+  const username = page.locator('input[name="username"]');
+
+  const password = page.locator('input[name="password"]');
+
+  await expect(username).toBeVisible();
+  await expect(username).toBeEnabled();
+  await expect(password).toBeVisible();
+  await expect(password).toBeEnabled();
+
+  const loginButton = page.locator('button[type="submit"]');
+
+  await expect(loginButton).toBeVisible();
+  await expect(loginButton).toBeEnabled();
+  await expect(username).toHaveAttribute('placeholder', 'Username');
+  
+  await expect(password).toHaveAttribute('placeholder', 'Password');
+  await username.fill('Admin');
+  await password.fill('admin123');
+  await loginButton.click();
+ 
+});
--- tests/orange.spec.js
+++ tests/orange.spec.js
@@ -11,3 +11,18 @@ test('Orange',async ({ page }) => {
 
   
 })
+
+// import { test, expect } from '@playwright/test';
+
+// test('test', async ({ page }) => {
+//   await page1.goto('https://www.flipkart.com/');
+//   await page1.getByRole('link', { name: 'Image Image Fastrack' }).click();
+//   const page2Promise = page1.waitForEvent('popup');
+//   await page1.locator('div:nth-child(4) > ._75nlfW > div:nth-child(4) > ._1sdMkc > .rPDeLR').click();
+//   const page2 = await page2Promise;
+//   await page2.getByRole('button', { name: 'Buy Now' }).click();
+//   await page2.getByRole('textbox').click();
+//   await page2.getByRole('textbox').fill('9344739419');
+//   await page2.getByRole('button', { name: 'CONTINUE' }).click();
+// });
+
